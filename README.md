c71c08ad5209eb69cdbeb0d57177ae4aacbbc7fb
Thomas Mays <thomas@mindofthomas.com>
2018-04-02 00:18:32 -0400

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# `nn-ts`
+
+A fork of [Daniel Shiffman](https://github.com/shiffman)'s [Toy Neural Network](https://github.com/CodingTrain/Toy-Neural-Network-JS), refactored in Typescript, with some improvements(/bugs) and new goodies as I need them in my own projects.
+
+This is just Typescript, no JS here. I recommend compiling it using [`typescript`](https://www.npmjs.com/package/typescript) if you need a JavaScript build.
+
+## TODO
+
+To be honest I don't know what I'm doing when it comes to actually building NN libraries, so my goal is to improve this library **and** not break it.
+
+- Performance improvements.
+	- I'm looking for a matrix library that I can swap in with the least amount of effort.
+- Clustering support
+	- I think it could be great to be able to train the NN as a cluster of multiple NNs, with 1 master NN (using Web Workers for each "child" NN would allow training to go by a lot faster in parallel).
+	- I'll do my best to make sure that the results of the NN when clustering is not changed.
---
+++ matrix.ts
@@ -0,0 +1,141 @@
+type MapFunc = (x: number, y: number, z: number) => number;
+type SMapFunc = (x: number, y: number, z: number) => number[];
+
+class Matrix {
+	rows: number;
+	cols: number;
+	data: number[][];
+
+	constructor(rows: number, cols: number) {
+		this.rows = rows;
+		this.cols = cols;
+
+		this.data = Array(this.rows).fill(null).map(() => Array(this.cols).fill(0));
+	}
+
+	copy() {
+		let m = new Matrix(this.rows, this.cols);
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				m.data[i][j] = this.data[i][j];
+			}
+		}
+		return m;
+	}
+
+	static fromArray(arr: number[]) {
+		return new Matrix(arr.length, 1).map((_, i) => arr[i]);
+	}
+
+	static subtract(a: Matrix, b: Matrix) {
+		if (a.rows !== b.rows || a.cols !== b.cols) {
+			console.log('Columns and Rows of A must match Columns and Rows of B.');
+			return;
+		}
+
+		// Return a new Matrix a-b
+		return new Matrix(a.rows, a.cols)
+			.map((_, i, j) => a.data[i][j] - b.data[i][j]);
+	}
+
+	toArray() {
+		let arr = [];
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				arr.push(this.data[i][j]);
+			}
+		}
+		return arr;
+	}
+
+	randomize() {
+		return this.map(e => Math.random() * 2 - 1);
+	}
+
+	add(n: number | Matrix) {
+		if (n instanceof Matrix) {
+			if (this.rows !== n.rows || this.cols !== n.cols) {
+				console.log('Columns and Rows of A must match Columns and Rows of B.');
+				return;
+			}
+			return this.map((e, i, j) => e + n.data[i][j]);
+		} else {
+			return this.map(e => e + n);
+		}
+	}
+
+	static transpose(matrix: Matrix) {
+		return new Matrix(matrix.cols, matrix.rows)
+			.map((_, i, j) => matrix.data[j][i]);
+	}
+
+	static multiply(a: Matrix, b: Matrix) {
+		// Matrix product
+		if (a.cols !== b.rows) {
+			console.log('Columns of A must match rows of B.')
+			return;
+		}
+
+		return new Matrix(a.rows, b.cols)
+			.map((e, i, j) => {
+				// Dot product of values in col
+				let sum = 0;
+				for (let k = 0; k < a.cols; k++) {
+					sum += a.data[i][k] * b.data[k][j];
+				}
+				return sum;
+			});
+	}
+
+	multiply(n: number | Matrix) {
+		if (n instanceof Matrix) {
+			if (this.rows !== n.rows || this.cols !== n.cols) {
+				console.log('Columns and Rows of A must match Columns and Rows of B.');
+				return;
+			}
+
+			// hadamard product
+			return this.map((e, i, j) => e * n.data[i][j]);
+		} else {
+			// Scalar product
+			return this.map(e => e * n);
+		}
+	}
+
+	map(func: MapFunc) {
+		// Apply a function to every element of matrix
+		for (let i = 0; i < this.rows; i++) {
+			for (let j = 0; j < this.cols; j++) {
+				let val = this.data[i][j];
+				this.data[i][j] = func(val, i, j);
+			}
+		}
+		return this;
+	}
+
+	static map(matrix: Matrix, func: MapFunc) {
+		// Apply a function to every element of matrix
+		return new Matrix(matrix.rows, matrix.cols)
+			.map((_, i, j) => func(matrix.data[i][j], i, j));
+	}
+
+	print() {
+		console.table(this.data);
+		return this;
+	}
+
+	serialize() {
+		return JSON.stringify(this);
+	}
+
+	static deserialize(data: any) {
+		if (typeof data == 'string') {
+			data = JSON.parse(data);
+		}
+		let matrix = new Matrix(data.rows, data.cols);
+		matrix.data = data.data;
+		return matrix;
+	}
+}
+
+export default Matrix;
